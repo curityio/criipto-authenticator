ee25978fae5ba616567071c2d29201ca9717392d
Atif Saddique <atif.saddique4@gmail.com>
2018-02-09 19:10:52 +0500

    first commit

---
+++ README.rst
@@ -0,0 +1,89 @@
+Criipto Authenticator Plug-in
+=============================
+
+.. image:: https://travis-ci.org/curityio/criipto-authenticator.svg?branch=master
+      :target: https://travis-ci.org/curityio/criipto-authenticator
+
+This project provides an opens source Criipto Authenticator plug-in for the Curity Identity Server. This allows an administrator to add functionality to Curity which will then enable end users to login using their Criipto credentials. The app that integrates with Curity may also be configured to receive the Criipto access token, allowing it to manage resources in a Criipto.
+
+System Requirements
+~~~~~~~~~~~~~~~~~~~
+
+* Curity Identity Server 2.4.0 and `its system requirements <https://developer.curity.io/docs/latest/system-admin-guide/system-requirements.html>`_
+
+Requirements for Building from Source
+"""""""""""""""""""""""""""""""""""""
+
+* Maven 3
+* Java JDK v. 8
+
+Compiling the Plug-in from Source
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+The source is very easy to compile. To do so from a shell, issue this command: ``mvn package``.
+
+Installation
+~~~~~~~~~~~~
+
+To install this plug-in, either download a binary version available from the `releases section of this project's GitHub repository <https://github.com/curityio/criipto-authenticator/releases>`_ or compile it from source (as described above). If you compiled the plug-in from source, the package will be placed in the ``target`` subdirectory. The resulting JAR file or the one downloaded from GitHub needs to placed in the directory ``${IDSVR_HOME}/usr/share/plugins/criipto``. (The name of the last directory, ``criipto``, which is the plug-in group, is arbitrary and can be anything.) After doing so, the plug-in will become available as soon as the node is restarted.
+
+.. note::
+
+    The JAR file needs to be deployed to each run-time node and the admin node. For simple test deployments where the admin node is a run-time node, the JAR file only needs to be copied to one location.
+
+For a more detailed explanation of installing plug-ins, refer to the `Curity developer guide <https://developer.curity.io/docs/latest/developer-guide/plugins/index.html#plugin-installation>`_.
+
+Creating a Criipto Authenticator in Curity
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+The easiest way to configure a new Criipto authenticator is using the Curity admin UI. The configuration for this can be downloaded as XML or CLI commands later, so only the steps to do this in the GUI will be described.
+
+1. Go to the ``Authenticators`` page of the authentication profile wherein the authenticator instance should be created.
+2. Click the ``New Authenticator`` button.
+3. Enter a name (e.g., ``criipto1``). This name needs to match the URI component in the callback URI set in the Criipto app.
+4. For the type, pick the ``Criipto`` option:
+
+    .. figure:: docs/images/criipto-authenticator-type-in-curity.png
+        :align: center
+        :width: 600px
+
+5. On the next page, you can define all of the standard authenticator configuration options like any previous authenticator that should run, the resulting ACR, transformers that should executed, etc. At the bottom of the configuration page, the Criipto-specific options can be found.
+
+    .. note::
+
+    The Criipto-specific configuration is generated dynamically based on the `configuration model defined in the Java interface <https://criipto.com/curityio/criipto-authenticator/blob/master/src/main/java/io/curity/identityserver/plugin/criipto/config/CriiptoAuthenticatorPluginConfig.java>`_.
+
+6. Certain required and optional configuration settings may be provided. One of these is the ``HTTP Client`` setting. This is the HTTP client that will be used to communicate with the Criipto OAuth server's token and user info endpoints. To define this, do the following:
+
+    A. click the ``Facilities`` button at the top-right of the screen.
+    B. Next to ``HTTP``, click ``New``.
+    C. Enter some name (e.g., ``criiptoClient``).
+    D. Click ``Apply``.
+
+        .. figure:: docs/images/criipto-http-client.png
+                :align: center
+                :width: 400px
+
+7. Back in the Criipto authenticator instance that you started to define, select the new HTTP client from the dropdown.
+
+        .. figure:: docs/images/http-client.png
+
+8. In the ``Client ID`` textfield, enter the client ID from the Criipto app configuration.
+9. Also enter the matching ``Client Secret``.
+10. If you wish to limit the scopes that Curity will request of Criipto, select the desired scopes from dropdown.
+
+Once all of these changes are made, they will be staged, but not committed (i.e., not running). To make them active, click the ``Commit`` menu option in the ``Changes`` menu. Optionally enter a comment in the ``Deploy Changes`` dialogue and click ``OK``.
+
+Once the configuration is committed and running, the authenticator can be used like any other.
+
+License
+~~~~~~~
+
+This plugin and its associated documentation is listed under the `Apache 2 license <LICENSE>`_.
+
+More Information
+~~~~~~~~~~~~~~~~
+
+Please visit `curity.io <https://curity.io/>`_ for more information about the Curity Identity Server.
+
+Copyright (C) 2017 Curity AB.
---
+++ src/main/java/io/curity/identityserver/plugin/criipto/authentication/CallbackRequestHandler.java
@@ -0,0 +1,240 @@
+/*
+ *  Copyright 2017 Curity AB
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
+package io.curity.identityserver.plugin.criipto.authentication;
+
+import io.curity.identityserver.plugin.criipto.config.CriiptoAuthenticatorPluginConfig;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import se.curity.identityserver.sdk.Nullable;
+import se.curity.identityserver.sdk.attribute.Attribute;
+import se.curity.identityserver.sdk.attribute.Attributes;
+import se.curity.identityserver.sdk.attribute.AuthenticationAttributes;
+import se.curity.identityserver.sdk.attribute.ContextAttributes;
+import se.curity.identityserver.sdk.attribute.SubjectAttributes;
+import se.curity.identityserver.sdk.authentication.AuthenticationResult;
+import se.curity.identityserver.sdk.authentication.AuthenticatorRequestHandler;
+import se.curity.identityserver.sdk.errors.ErrorCode;
+import se.curity.identityserver.sdk.http.HttpRequest;
+import se.curity.identityserver.sdk.http.HttpResponse;
+import se.curity.identityserver.sdk.service.ExceptionFactory;
+import se.curity.identityserver.sdk.service.HttpClient;
+import se.curity.identityserver.sdk.service.Json;
+import se.curity.identityserver.sdk.service.WebServiceClient;
+import se.curity.identityserver.sdk.service.WebServiceClientFactory;
+import se.curity.identityserver.sdk.service.authentication.AuthenticatorInformationProvider;
+import se.curity.identityserver.sdk.web.Request;
+import se.curity.identityserver.sdk.web.Response;
+
+import java.io.UnsupportedEncodingException;
+import java.net.URI;
+import java.net.URLEncoder;
+import java.nio.charset.StandardCharsets;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+import java.util.Objects;
+import java.util.Optional;
+
+public class CallbackRequestHandler implements AuthenticatorRequestHandler<CallbackGetRequestModel>
+{
+    private final static Logger _logger = LoggerFactory.getLogger(CallbackRequestHandler.class);
+
+    private final ExceptionFactory _exceptionFactory;
+    private final CriiptoAuthenticatorPluginConfig _config;
+    private final Json _json;
+    private final AuthenticatorInformationProvider _authenticatorInformationProvider;
+    private final WebServiceClientFactory _webServiceClientFactory;
+
+    public CallbackRequestHandler(CriiptoAuthenticatorPluginConfig config)
+    {
+        _exceptionFactory = config.getExceptionFactory();
+        _config = config;
+        _json = config.getJson();
+        _webServiceClientFactory = config.getWebServiceClientFactory();
+        _authenticatorInformationProvider = config.getAuthenticatorInformationProvider();
+    }
+
+    @Override
+    public CallbackGetRequestModel preProcess(Request request, Response response)
+    {
+        if (request.isGetRequest())
+        {
+            return new CallbackGetRequestModel(request);
+        }
+        else
+        {
+            throw _exceptionFactory.methodNotAllowed();
+        }
+    }
+
+    @Override
+    public Optional<AuthenticationResult> post(CallbackGetRequestModel requestModel, Response response)
+    {
+        throw _exceptionFactory.methodNotAllowed();
+    }
+
+    @Override
+    public Optional<AuthenticationResult> get(CallbackGetRequestModel requestModel, Response response)
+    {
+        validateState(requestModel.getState());
+        handleError(requestModel);
+
+        Map<String, Object> tokenResponseData = redeemCodeForTokens(requestModel);
+        @Nullable Object accessToken = tokenResponseData.get("access_token");
+        List<Attribute> subjectAttributes = new LinkedList<>(), contextAttributes = new LinkedList<>();
+        String userId = tokenResponseData.get("user_id").toString();
+
+        subjectAttributes.add(Attribute.of("user_id", userId));
+
+        contextAttributes.add(Attribute.of("criipto_access_token", accessToken.toString()));
+        contextAttributes.add(Attribute.of("granted_scopes", tokenResponseData.get("scope").toString()));
+
+        AuthenticationAttributes authenticationAttributes = AuthenticationAttributes.of(
+                SubjectAttributes.of(userId, Attributes.of(subjectAttributes)),
+                ContextAttributes.of(contextAttributes));
+
+        return Optional.of(new AuthenticationResult(authenticationAttributes));
+    }
+
+    private Map<String, Object> redeemCodeForTokens(CallbackGetRequestModel requestModel)
+    {
+        HttpResponse tokenResponse = getWebServiceClient()
+                .withPath("/api/oauth.access")
+                .request()
+                .contentType("application/x-www-form-urlencoded")
+                .body(getFormEncodedBodyFrom(createPostData(_config.getClientId(), _config.getClientSecret(),
+                        requestModel.getCode(), requestModel.getRequestUrl())))
+                .method("POST")
+                .response();
+        int statusCode = tokenResponse.statusCode();
+
+        if (statusCode != 200)
+        {
+            if (_logger.isInfoEnabled())
+            {
+                _logger.info("Got error response from token endpoint: error = {}, {}", statusCode,
+                        tokenResponse.body(HttpResponse.asString()));
+            }
+
+            throw _exceptionFactory.internalServerException(ErrorCode.EXTERNAL_SERVICE_ERROR);
+        }
+
+        return _json.fromJson(tokenResponse.body(HttpResponse.asString()));
+    }
+
+    private WebServiceClient getWebServiceClient()
+    {
+        Optional<HttpClient> httpClient = _config.getHttpClient();
+
+        if (httpClient.isPresent())
+        {
+            return _webServiceClientFactory.create(httpClient.get()).withHost("criipto.com");
+        }
+        else
+        {
+            return _webServiceClientFactory.create(URI.create("https://criipto.com"));
+        }
+    }
+
+    private void handleError(CallbackGetRequestModel requestModel)
+    {
+        if (!Objects.isNull(requestModel.getError()))
+        {
+            if ("access_denied".equals(requestModel.getError()))
+            {
+                _logger.debug("Got an error from Criipto: {} - {}", requestModel.getError(), requestModel
+                        .getErrorDescription());
+
+                throw _exceptionFactory.redirectException(
+                        _authenticatorInformationProvider.getAuthenticationBaseUri().toASCIIString());
+            }
+
+            _logger.warn("Got an error from Criipto: {} - {}", requestModel.getError(), requestModel
+                    .getErrorDescription());
+
+            throw _exceptionFactory.externalServiceException("Login with Criipto failed");
+        }
+    }
+
+    private static Map<String, String> createPostData(String clientId, String clientSecret, String code, String
+            callbackUri)
+    {
+        Map<String, String> data = new HashMap<>(5);
+
+        data.put("client_id", clientId);
+        data.put("client_secret", clientSecret);
+        data.put("code", code);
+        data.put("grant_type", "authorization_code");
+        data.put("redirect_uri", callbackUri);
+
+        return data;
+    }
+
+    private static HttpRequest.BodyProcessor getFormEncodedBodyFrom(Map<String, String> data)
+    {
+        StringBuilder stringBuilder = new StringBuilder();
+
+        data.entrySet().forEach(e -> appendParameter(stringBuilder, e));
+
+        return HttpRequest.fromString(stringBuilder.toString());
+    }
+
+    private static void appendParameter(StringBuilder stringBuilder, Map.Entry<String, String> entry)
+    {
+        String key = entry.getKey();
+        String value = entry.getValue();
+        String encodedKey = urlEncodeString(key);
+        stringBuilder.append(encodedKey);
+
+        if (!Objects.isNull(value))
+        {
+            String encodedValue = urlEncodeString(value);
+            stringBuilder.append("=").append(encodedValue);
+        }
+
+        stringBuilder.append("&");
+    }
+
+    private static String urlEncodeString(String unencodedString)
+    {
+        try
+        {
+            return URLEncoder.encode(unencodedString, StandardCharsets.UTF_8.name());
+        }
+        catch (UnsupportedEncodingException e)
+        {
+            throw new RuntimeException("This server cannot support UTF-8!", e);
+        }
+    }
+
+    private void validateState(String state)
+    {
+        @Nullable Attribute sessionAttribute = _config.getSessionManager().get("state");
+
+        if (sessionAttribute != null && state.equals(sessionAttribute.getValueOfType(String.class)))
+        {
+            _logger.debug("State matches session");
+        }
+        else
+        {
+            _logger.debug("State did not match session");
+
+            throw _exceptionFactory.badRequestException(ErrorCode.INVALID_SERVER_STATE, "Bad state provided");
+        }
+    }
+}
